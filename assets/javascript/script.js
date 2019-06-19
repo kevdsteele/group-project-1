bb13a144c407fcf6d8396a752735b3bf5449ac0b
Kevin Steele <kevdsteele@gmail.com>
2019-06-19 12:33:53 -0400

    correct url

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 
 
-    var url ="https://sandbox-api.brewerydb.com/v2&key=e90031852e06a3a450353766009ce514";
+    var url ="https://sandbox-api.brewerydb.com/v2/?key=e90031852e06a3a450353766009ce514";
     $.ajax({
       url: url,
       method: "GET"
