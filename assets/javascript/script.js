8f16de8e1dd6e2eef42b86f39c0db417eaaf7c5c
Kevin Steele <kevdsteele@gmail.com>
2019-06-19 13:18:36 -0400

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
           "Cache-Control": "no-cache",
           "Postman-Token": "f64f5947-0b4e-4d8a-98ee-35a81486d940,867e39a0-294e-4f07-8f73-b51f8ef76db5",
           "accept-encoding": "gzip, deflate",
-          "referer": "http://sandbox-api.brewerydb.com/v2/?e90031852e06a3a450353766009ce514=",
+          "referer": "https://sandbox-api.brewerydb.com/v2/?e90031852e06a3a450353766009ce514=",
           "Connection": "keep-alive",
           "cache-control": "no-cache"
         }
