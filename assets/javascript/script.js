999c9cf7a376cec35656c3a4d1df7a2e530d7d2e
Kevin Steele <kevdsteele@gmail.com>
2019-06-19 13:17:05 -0400

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,14 +3,26 @@ $(document).ready(function () {
 
 
 
-    var url ="https://sandbox-api.brewerydb.com/v2/?key=e90031852e06a3a450353766009ce514";
-    $.ajax({
-      url: url,
-      method: "GET"
-    }).then(function (response) {
-      console.log(response);
-    } ); 
-
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://sandbox-api.brewerydb.com/v2?e90031852e06a3a450353766009ce514=",
+        "method": "GET",
+        "headers": {
+          "User-Agent": "PostmanRuntime/7.15.0",
+          "Accept": "*/*",
+          "Cache-Control": "no-cache",
+          "Postman-Token": "f64f5947-0b4e-4d8a-98ee-35a81486d940,867e39a0-294e-4f07-8f73-b51f8ef76db5",
+          "accept-encoding": "gzip, deflate",
+          "referer": "http://sandbox-api.brewerydb.com/v2/?e90031852e06a3a450353766009ce514=",
+          "Connection": "keep-alive",
+          "cache-control": "no-cache"
+        }
+      }
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
 
 
 
